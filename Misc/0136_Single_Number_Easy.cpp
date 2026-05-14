1f9c204bd381dc16082b1ad6695740d7cfb2d0a2
z8106007005-hub <z8106007005@gmail.com>
2026-05-14 23:40:08 +0530

    Solved: 0136. Single Number [cpp]

--- Misc/0136_Single_Number_Easy.cpp
+++ Misc/0136_Single_Number_Easy.cpp
@@ -23,4 +23,27 @@ public:
 
         return freq[0].first ;
     }
+};/*
+ * ------------------------------------------------------------
+ * LeetSync appended newer accepted solution
+ * LeetSync Update Marker: submission:2000490732
+ * Appended At: 2026-05-14T18:10:06.711Z
+ * ------------------------------------------------------------
+ *//**
+ * Problem: 0136. Single Number
+ * Difficulty: Easy
+ * URL: https://leetcode.com/problems/single-number/
+ * Language: cpp
+ * Synced: 2026-05-14T18:10:06.244Z
+ */
+/* LeetSync Update Marker: submission:2000490732 */
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin() , nums.end()) ; 
+        for(int i = 1 ; i < nums.size() ; i += 2) {
+            if(nums[i] != nums[i - 1]) return nums[i - 1] ; 
+        }
+        return nums[nums.size() - 1] ;
+    }
 };
