b8df8c74ed75cbc9819a9ec6da27ea9c76927e81
z8106007005-hub <z8106007005@gmail.com>
2026-05-14 23:31:04 +0530

    Solved: 0050. Pow(x, n) [cpp]

---
+++ Misc/0050_Pow(x,_n)_Medium.cpp
@@ -0,0 +1,14 @@
+/**
+ * Problem: 0050. Pow(x, n)
+ * Difficulty: Medium
+ * URL: https://leetcode.com/problems/powx-n/
+ * Language: cpp
+ * Synced: 2026-05-14T18:01:02.820Z
+ */
+/* LeetSync Update Marker: submission:1861257308 */
+class Solution {
+public:
+    double myPow(double x, int n) {
+        return pow(x , n) ;
+    }
+};
