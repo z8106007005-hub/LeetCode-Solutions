03b6e541e8a982fe4f99a7ce5cf86ffb987d4581
z8106007005-hub <z8106007005@gmail.com>
2026-05-14 23:39:47 +0530

    Solved: 0189. Rotate Array [cpp]

--- Misc/0189_Rotate_Array_Medium.cpp
+++ Misc/0189_Rotate_Array_Medium.cpp
@@ -17,4 +17,107 @@ public:
         reverse(a.begin() , a.begin() + k) ;
         reverse(a.begin() + k , a.end()) ;
     }
+};/*
+ * ------------------------------------------------------------
+ * LeetSync appended newer accepted solution
+ * LeetSync Update Marker: submission:1999995925
+ * Appended At: 2026-05-14T18:09:46.070Z
+ * ------------------------------------------------------------
+ *//**
+ * Problem: 0189. Rotate Array
+ * Difficulty: Medium
+ * URL: https://leetcode.com/problems/rotate-array/
+ * Language: cpp
+ * Synced: 2026-05-14T18:09:45.608Z
+ */
+/* LeetSync Update Marker: submission:1999995925 */
+#pragma GCC optimize("O3")
+#pragma GCC optimize(2)
+#pragma GCC optimize(3)
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("inline")
+#pragma GCC optimize("-fgcse")
+#pragma GCC optimize("-fgcse-lm")
+#pragma GCC optimize("-fipa-sra")
+#pragma GCC optimize("-ftree-pre")
+#pragma GCC optimize("-ftree-vrp")
+#pragma GCC optimize("-fpeephole2")
+#pragma GCC optimize("-ffast-math")
+#pragma GCC optimize("-fsched-spec")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("-falign-jumps")
+#pragma GCC optimize("-falign-loops")
+#pragma GCC optimize("-falign-labels")
+#pragma GCC optimize("-fdevirtualize")
+#pragma GCC optimize("-fcaller-saves")
+#pragma GCC optimize("-fcrossjumping")
+#pragma GCC optimize("-fthread-jumps")
+#pragma GCC optimize("-funroll-loops")
+#pragma GCC optimize("-freorder-blocks")
+#pragma GCC optimize("-fschedule-insns")
+#pragma GCC optimize("inline-functions")
+#pragma GCC optimize("-ftree-tail-merge")
+#pragma GCC optimize("-fschedule-insns2")
+#pragma GCC optimize("-fstrict-aliasing")
+#pragma GCC optimize("-falign-functions")
+#pragma GCC optimize("-fcse-follow-jumps")
+#pragma GCC optimize("-fsched-interblock")
+#pragma GCC optimize("-fpartial-inlining")
+#pragma GCC optimize("no-stack-protector")
+#pragma GCC optimize("-freorder-functions")
+#pragma GCC optimize("-findirect-inlining")
+#pragma GCC optimize("-fhoist-adjacent-loads")
+#pragma GCC optimize("-frerun-cse-after-loop")
+#pragma GCC optimize("inline-small-functions")
+#pragma GCC optimize("-finline-small-functions")
+#pragma GCC optimize("-ftree-switch-conversion")
+#pragma GCC optimize("-foptimize-sibling-calls")
+#pragma GCC optimize("-fexpensive-optimizations")
+#pragma GCC optimize("inline-functions-called-once")
+#pragma GCC optimize("-fdelete-null-pointer-checks")
+
+const size_t BUFFER_SIZE = 0x30000000;
+alignas(64) static char buffer[BUFFER_SIZE];
+static size_t buffer_pos = 0;
+
+__attribute__((hot, always_inline))
+void* operator new(size_t size) {
+    size_t aligned_pos = (buffer_pos + 63) & ~63;
+    char* ptr = buffer + aligned_pos;
+    buffer_pos = aligned_pos + size;
+    return ptr;
+}
+
+__attribute__((hot, always_inline))
+void* operator new[](size_t size) {
+    return operator new(size);
+}
+
+__attribute__((hot, always_inline))
+void operator delete(void*) noexcept {}
+__attribute__((hot, always_inline))
+void operator delete(void*, size_t) noexcept {}
+__attribute__((hot, always_inline))
+void operator delete[](void*) noexcept {}
+__attribute__((hot, always_inline))
+void operator delete[](void*, size_t) noexcept {}
+
+__attribute__((constructor))
+static void fast_io() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+}
+
+class Solution {
+public:
+    void rotate(vector<int>& a ,int k) {
+
+        // if k > n then 
+        k = k % a.size() ; 
+        //divide and conquor
+        reverse(a.begin() , a.end()) ; 
+        reverse(a.begin() , a.begin() + k) ;
+        reverse(a.begin() + k , a.end()) ;
+    }
 };
