e721e8648868cb88e1c91baab11bd7d416966b91
z8106007005-hub <z8106007005@gmail.com>
2026-05-14 23:37:40 +0530

    Solved: 0070. Climbing Stairs [cpp]

--- Misc/0070_Climbing_Stairs_Easy.cpp
+++ Misc/0070_Climbing_Stairs_Easy.cpp
@@ -25,4 +25,35 @@ public:
     }
 };
 
+auto init=atexit([]{ofstream("display_runtime.txt")<<"0";});/*
+ * ------------------------------------------------------------
+ * LeetSync appended newer accepted solution
+ * LeetSync Update Marker: submission:1965439295
+ * Appended At: 2026-05-14T18:07:39.152Z
+ * ------------------------------------------------------------
+ *//**
+ * Problem: 0070. Climbing Stairs
+ * Difficulty: Easy
+ * URL: https://leetcode.com/problems/climbing-stairs/
+ * Language: cpp
+ * Synced: 2026-05-14T18:07:38.794Z
+ */
+/* LeetSync Update Marker: submission:1965439295 */
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n <= 2) return n ;
+
+        vector<int> dp(n) ;
+        dp[0] = 1 ; 
+        dp[1] = 2 ; 
+        for(int i = 2 ; i < n ; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] ; 
+        }
+
+        return dp[n - 1] ;
+        //return climbStairs(n - 1) + climbStairs(n - 2) ; 
+    }
+};
+
 auto init=atexit([]{ofstream("display_runtime.txt")<<"0";});
