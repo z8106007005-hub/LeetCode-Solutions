d32a7ca9d54e301fd6416b49a6900449376f97d4
z8106007005-hub <z8106007005@gmail.com>
2026-05-14 23:52:26 +0530

    Solved: 1552. Magnetic Force Between Two Balls [cpp]

--- Misc/1552_Magnetic_Force_Between_Two_Balls_Medium.cpp
+++ Misc/1552_Magnetic_Force_Between_Two_Balls_Medium.cpp
@@ -87,4 +87,52 @@ public:
         return ans ;
     }
 };
-auto init=atexit([]{ofstream("display_runtime.txt")<<"0";});
+auto init=atexit([]{ofstream("display_runtime.txt")<<"0";});/*
+ * ------------------------------------------------------------
+ * LeetSync appended newer accepted solution
+ * LeetSync Update Marker: submission:1876716554
+ * Appended At: 2026-05-14T18:22:24.259Z
+ * ------------------------------------------------------------
+ *//**
+ * Problem: 1552. Magnetic Force Between Two Balls
+ * Difficulty: Medium
+ * URL: https://leetcode.com/problems/magnetic-force-between-two-balls/
+ * Language: cpp
+ * Synced: 2026-05-14T18:22:23.880Z
+ */
+/* LeetSync Update Marker: submission:1876716554 */
+#define ll int 
+class Solution {
+public:
+
+    bool poss(ll mid , vector<ll>& pos , ll m) {
+        ll last = pos[0] ;
+        m-- ;
+        for(ll i = 1 ; i < pos.size() ; i++) {
+            if(pos[i] - last >= mid) {
+                m-- ;
+                last = pos[i] ;
+            }
+            if(m == 0) return true ;
+        }
+        return false ;
+    }
+
+    int maxDistance(vector<int>& pos , int m) {
+        sort(pos.begin() , pos.end()) ;
+        ll n = pos.size() ;
+        ll low = 1 ; 
+        ll high = pos[n - 1] - pos[0] ;
+        ll ans = 1 ;
+        while(low <= high) {
+            ll mid = (low + high) / 2 ;
+            if(poss(mid , pos , m)) {
+                ans = mid ; 
+                low = mid + 1 ;
+            }
+            else high = mid - 1 ;
+        }
+        return ans ;
+    }
+};
+ auto init=atexit([]{ofstream("display_runtime.txt")<<"0";});
