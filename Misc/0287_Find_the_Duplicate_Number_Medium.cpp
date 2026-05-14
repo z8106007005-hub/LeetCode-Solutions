36d648334de87c8b645d784f3210680f07c9d0b4
z8106007005-hub <z8106007005@gmail.com>
2026-05-14 23:31:27 +0530

    Solved: 0287. Find the Duplicate Number [cpp]

---
+++ Misc/0287_Find_the_Duplicate_Number_Medium.cpp
@@ -0,0 +1,26 @@
+/**
+ * Problem: 0287. Find the Duplicate Number
+ * Difficulty: Medium
+ * URL: https://leetcode.com/problems/find-the-duplicate-number/
+ * Language: cpp
+ * Synced: 2026-05-14T18:01:25.590Z
+ */
+/* LeetSync Update Marker: submission:1861739148 */
+#define ll int 
+class Solution {
+public:
+    int findDuplicate(vector<int>& a) {
+        int n = a.size() ;
+        unordered_map<ll , ll> mp ;
+        for(ll i = 0 ; i < n ; i++) {
+            mp[a[i]]++ ;
+        }
+        vector<pair<ll , ll>> b(mp.begin() , mp.end()) ;
+        sort(b.rbegin() , b.rend() , [](const pair<ll , ll> &x , const pair<ll , ll> &y)
+        {
+            return x.second < y.second ;
+        }) ;
+
+        return b[0].first ;
+    }
+};
